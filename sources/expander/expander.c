ee658701b4326b2614cc10381b90887f629d7a38
Evan Morvan <emorvan@student.42nice.fr>
2022-11-26 11:09:05 +0100

    expander WIP

--- sources/expander/expander.c
+++ sources/expander/expander.c
@@ -6,16 +6,17 @@
 /*   By: emorvan <emorvan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 14:29:07 by emorvan           #+#    #+#             */
-/*   Updated: 2022/11/23 14:33:33 by emorvan          ###   ########.fr       */
+/*   Updated: 2022/11/26 11:08:45 by emorvan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-// Turns $ENV into 
-char    *expand_env(char *dollar_sign)
+// Expand $VARIABLES to their value
+char	*expand_env(char *dollar_sign)
 {
-    char    *env_variable;
+	char	*env_variable;
 
-    env_variable = getenv(ft_substr(dollar_sign, 1, ft_strlen(dollar_sign)));
+	env_variable = getenv(ft_substr(dollar_sign, 1, ft_strlen(dollar_sign)));
+	return (env_variable);
 }
